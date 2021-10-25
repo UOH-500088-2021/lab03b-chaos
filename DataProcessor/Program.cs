9162097ca4323eff5d9748be8e35257e78ddbdd3
DANIEL BULMAN <D.BULMAN-2020@hull.ac.uk>
2021-10-25 10:43:49 +0100

    Added min and max and fixed code

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -15,6 +15,8 @@ namespace DataProcessor
 
             Console.WriteLine(String.Format("Average total calls: {0}", dataReader.AverageTotalCalls));
             Console.WriteLine(String.Format("Number of rows: {0}", dataReader.NumberOfForces));
+            Console.WriteLine("Minimum value of number of calls " + dataReader.MinNumberOfCalls);
+            Console.WriteLine("Maxium value of numnber of calls " + dataReader.MaxNumberOfCalls);
             Console.WriteLine("Press enter key to exit");
             Console.ReadLine();
         }
@@ -26,7 +28,8 @@ namespace DataProcessor
 
         public int AverageTotalCalls { get; set; }
         public int NumberOfForces { get; set; }
-
+        public int MinNumberOfCalls { get; set; }
+        public int MaxNumberOfCalls { get; set; }
         public void ReadData()
         {
             var totalCallsColumn = 2;
@@ -34,16 +37,33 @@ namespace DataProcessor
             {
                 var totalCalls = 0;
                 var line = reader.ReadLine(); // Read the header line, which doesn't contain data
+                MaxNumberOfCalls = 0;
+                MinNumberOfCalls = 999999999;
                 while (!reader.EndOfStream)
                 {
                     NumberOfForces++;
                     line = reader.ReadLine();
                     var columns = line.Split(",");  // What if there's a comma inside a data column?
-                    totalCalls += int.Parse(columns[1]);  // What if the data doesn't parse properly?
+                    foreach (string number in columns)
+                    {
+                        bool passed = int.TryParse(number, out int calls);
+                        if (passed == true)
+                        {
+                            if (MaxNumberOfCalls < calls)
+                            {
+                                MaxNumberOfCalls = calls;
+                            }
+                            if (MinNumberOfCalls > calls)
+                            {
+                                MinNumberOfCalls = calls;
+                            }
+                            totalCalls += calls;
+                        } // What if the data doesn't parse properly?
+                    }
                 }
                 AverageTotalCalls = totalCalls / NumberOfForces;
             }
         }
     }
-        
+
 }
